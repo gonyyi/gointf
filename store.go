bd64e5e6c97a851a3a2317d2defb27aacebf2821
Gon <29622355+gonyyi@users.noreply.github.com>
2021-03-25 11:42:23 -0500

    Update store.go

--- store.go
+++ store.go
@@ -2,9 +2,12 @@ package gointf
 
 // Storer is an interface for DB connection
 type Storer interface {
+	NewBucket(bucket string) error
+	DeleteBucket(bucket string) error
+	Flush() error
+	
 	Put(bucket, key string, data []byte) error
 	Get(buckt, key string) ([]byte, error)
 	Delete(bucket, key string) error
 	Iterate(bucket, prefix string, fn func(key string, value []byte)) error
-	Flush() error
 }
