fa074a79b6f0516df48568eebf5e8af4b98837f2
Gon <29622355+gonyyi@users.noreply.github.com>
2021-03-29 10:20:07 -0500

    Update store.go

--- store.go
+++ store.go
@@ -2,12 +2,22 @@ package gointf
 
 // Storer is an interface for DB connection
 type Storer interface {
-	CreateBucket(bucket string) error
-	DeleteBucket(bucket string) error
+	Lock()
+	Unlock()
 	Flush() error
 	
-	Put(bucket, key string, data []byte) error
-	Get(buckt, key string) ([]byte, error)
-	Delete(bucket, key string) error
-	Iterate(bucket, prefix string, fn func(key string, value []byte)) error
+	NewBucket(bucket string) error
+	DelBucket(bucket string) error
+	
+	Get(bucket, key []byte) ([]byte, error)
+	Put(bucket, key, val []byte) error
+	Del(bucket, key string) error
+
+	// Do will get/update/date using func([]byte)([]byte,error). If returned []byte is nil,
+	// it means delete the key.
+	Do(bucket, key []byte, func(val []byte)([]byte, error))([]byte, error)
+	
+	// Do iter will take keyPrefix, and based on that iterate.
+	// When the func returns nil for []byte, it will delete the key.
+	DoIter(bucket, keyPrefix []byte, func(key, val []byte)([]byte, error)) error
 }
